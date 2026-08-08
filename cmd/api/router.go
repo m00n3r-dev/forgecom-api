667856b2750bbed3b69f0b7fb9816dad5dce32cf
m00n3r <jeewakasandeep2021@gmail.com>
2026-08-08 09:37:24 +0530

    Add logging middleware to track request duration and method

--- cmd/api/router.go
+++ cmd/api/router.go
@@ -5,6 +5,7 @@ import (
 	"github.com/m00n3r-dev/forgecom-api/internal/auth"
 	"github.com/m00n3r-dev/forgecom-api/internal/config"
 	"github.com/m00n3r-dev/forgecom-api/internal/database"
+	"github.com/m00n3r-dev/forgecom-api/internal/middleware"
 	"github.com/m00n3r-dev/forgecom-api/internal/user"
 )
 
@@ -15,6 +16,9 @@ func SetupRoutes(config *config.Config, db *database.DB) *chi.Mux {
 
 	// router
 	r := chi.NewRouter()
+
+	// middlewares
+	r.Use(middleware.Logger)
 
 	// dependencies
 	userRepository := user.NewRepository(db.DB)
